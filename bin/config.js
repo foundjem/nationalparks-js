9f05afdabbfaecaee0205f8d24e0c251e2248a2e
Foundjem <foundjem@users.noreply.github.com>
2018-05-20 15:07:33 -0700

    Changed to Park Jarry

--- bin/config.js
+++ bin/config.js
@@ -14,7 +14,7 @@ var autoconfig  = function (config_overrides){
 
   var ws_info = {
     id: "nationalparks-js",
-    displayName: "Park GLobal-- OpenStack.",
+    displayName: "Park Jarry -- Montreal.",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
     zoom: 4
